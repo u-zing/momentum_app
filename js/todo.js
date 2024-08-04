7f87cae8e97b362fa1edcce0aba71d8ff04c0f77
zing <156516215+u-zing@users.noreply.github.com>
2024-08-04 23:54:01 +0900

    css-ing(1)

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) {
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "X";
   button.addEventListener("click", deleteTodo);
   li.appendChild(span);
   li.appendChild(button);
