4e71c62a2694c0b4c48431771928f2a5f4086033
zing <156516215+u-zing@users.noreply.github.com>
2024-08-04 21:51:38 +0900

    JS final

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,17 @@ const API_KEY = "58c60e6ab429d91a5114bfaed88ff745";
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-fetch(url);
 function onGeoError() {
   alert("Can't find you,No weather for you.");
 }
